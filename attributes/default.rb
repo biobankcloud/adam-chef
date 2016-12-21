5497fa9ef232ed70cc4c97f6c74c2a46525e6170
Jim Dowling <jdowling@sics.se>
2016-12-21 17:21:53 +0100

    adam home dir fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default.adam.version             = "0.20.0"
 default.adam.user                = "#{node.hadoop_spark.user}"
 default.adam.group               = "#{node.hadoop_spark.group}"
 default.adam.dir                 = "/srv"
-default.adam.home                = "#{node.adam.dir}/adam-distribution_#{node.scala.version}-#{node.adam.version}"
+default.adam.home                = "#{node.adam.dir}/adam-distribution-spark2_#{node.scala.version}-#{node.adam.version}"
 default.adam.base_dir            = "#{node.adam.dir}/adam"
 # adam-distribution-spark2_2.11-0.20.0-bin.tar.gz
 default.adam.url                 = "#{default.download_url}/adam/adam-distribution-spark2_#{node.scala.version}-#{default.adam.version}-bin.tar.gz"
