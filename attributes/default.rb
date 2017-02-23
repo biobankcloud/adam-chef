1674669fdeabcb40044421e681329a5f2bf968f7
Jim Dowling <jdowling@sics.se>
2017-02-23 19:24:11 +0100

    change url for adam

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,6 +9,7 @@ default.adam.dir                 = "/srv"
 default.adam.home                = "#{node.adam.dir}/adam-distribution-spark2_#{node.scala.version}-#{node.adam.version}"
 default.adam.base_dir            = "#{node.adam.dir}/adam"
 # adam-distribution-spark2_2.11-0.20.0-bin.tar.gz
-default.adam.url                 = "#{default.download_url}/adam/adam-distribution-spark2_#{node.scala.version}-#{default.adam.version}-bin.tar.gz"
+# adam-distribution-spark2_2.11-0.21.0-bin
+default.adam.url                 = "#{default.download_url}/adam-distribution-spark2_#{node.scala.version}-#{default.adam.version}-bin.tar.gz"
 
 default.adam.jar                 = "adam_#{node.scala.version}-#{node.adam.version}.jar"
