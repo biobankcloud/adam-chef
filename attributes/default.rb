d176f821e6c0c8bdc3fb5b74d38c735eb2d361e3
Jim Dowling <jdowling@sics.se>
2017-03-06 08:50:08 +0100

    node.install.dir added

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default.scala.version            = "2.11"
 default.adam.version             = "0.21.0"
 default.adam.user                = "#{node.hadoop_spark.user}"
 default.adam.group               = "#{node.hadoop_spark.group}"
-default.adam.dir                 = "/srv"
+default.adam.dir                 = node.install.dir.empty? ? node.install.dir :  "/srv" 
 default.adam.home                = "#{node.adam.dir}/adam-distribution-spark2_#{node.scala.version}-#{node.adam.version}"
 default.adam.base_dir            = "#{node.adam.dir}/adam"
 # adam-distribution-spark2_2.11-0.20.0-bin.tar.gz
