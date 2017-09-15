f8bb7282f077985d8b1204fa965cc18d8a0793f5
Jim Dowling <jdowling@sics.se>
2017-09-15 21:49:34 +0200

    switch to spark2 version

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,10 +6,10 @@ default.adam.version             = "0.22.0"
 default.adam.user                = node.install.user.empty? ? node.hadoop_spark.user : node.install.user 
 default.adam.group               = node.install.user.empty? ? node.hadoop_spark.group : node.install.user 
 default.adam.dir                 = node.install.dir.empty? ?  "/srv"  : node.install.dir
-default.adam.home                = "#{node.adam.dir}/adam-distribution_#{node.scala.version}-#{node.adam.version}"
+default.adam.home                = "#{node.adam.dir}/adam-distribution-spark2_#{node.scala.version}-#{node.adam.version}"
 default.adam.base_dir            = "#{node.adam.dir}/adam"
 # https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution_2.11/0.22.0/adam-distribution_2.11-0.22.0-bin.tar.gz
-default.adam.url                 = "#{default.download_url}/adam-distribution_#{node.scala.version}-#{default.adam.version}-bin.tar.gz"
+default.adam.url                 = "#{default.download_url}/adam-distribution-spark2_#{node.scala.version}-#{default.adam.version}-bin.tar.gz"
 
 # adam_2.11-0.22.0.jar
 default.adam.jar                 = "adam_#{node.scala.version}-#{node.adam.version}.jar"
