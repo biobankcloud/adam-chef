0703ba971a1f6aba5862eeec4132c2cb0fdae343
karamel <jdowling@sics.se>
2017-09-17 00:51:31 +0200

    restart nm and dn after updating adam

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,7 +9,9 @@ default.adam.dir                 = node.install.dir.empty? ?  "/srv"  : node.ins
 default.adam.home                = "#{node.adam.dir}/adam-distribution-spark2_#{node.scala.version}-#{node.adam.version}"
 default.adam.base_dir            = "#{node.adam.dir}/adam"
 # https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution_2.11/0.22.0/adam-distribution_2.11-0.22.0-bin.tar.gz
-default.adam.url                 = "#{default.download_url}/adam-distribution-spark2_#{node.scala.version}-#{default.adam.version}-bin.tar.gz"
+default.adam.url                 = "#{node.download_url}/adam-distribution-spark2_#{node.scala.version}-#{default.adam.version}-bin.tar.gz"
 
 # adam_2.11-0.22.0.jar
 default.adam.jar                 = "adam_#{node.scala.version}-#{node.adam.version}.jar"
+
+default.adam.avro_jar            = "#{node.download_url}/avro-1.7.7.jar"
