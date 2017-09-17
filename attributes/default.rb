49dc8a54ea0392c3864ac27ee4678a98d2eecd06
karamel <jdowling@sics.se>
2017-09-17 10:25:40 +0200

    hops build  - use it

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ include_attribute "kagent"
 include_attribute "hadoop_spark"
 
 default.scala.version            = "2.11"
-default.adam.version             = "0.22.0"
+default.adam.version             = "0.23.0-SNAPSHOT"
 default.adam.user                = node.install.user.empty? ? node.hadoop_spark.user : node.install.user 
 default.adam.group               = node.install.user.empty? ? node.hadoop_spark.group : node.install.user 
 default.adam.dir                 = node.install.dir.empty? ?  "/srv"  : node.install.dir
@@ -12,6 +12,6 @@ default.adam.base_dir            = "#{node.adam.dir}/adam"
 default.adam.url                 = "#{node.download_url}/adam-distribution-spark2_#{node.scala.version}-#{default.adam.version}-bin.tar.gz"
 
 # adam_2.11-0.22.0.jar
-default.adam.jar                 = "adam_#{node.scala.version}-#{node.adam.version}.jar"
+default.adam.jar                 = "adam-assembly-spark2_#{node.scala.version}-#{node.adam.version}.jar"
 
 default.adam.avro_url            = "#{node.download_url}/avro-1.7.7.jar"
